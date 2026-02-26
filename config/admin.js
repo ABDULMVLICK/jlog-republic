d64bbaafa6cd984537b8dec38c2269d1d8868d8d
malick <abdulmvlick@gmail.com>
2026-02-27 00:06:55 +0100

    fix: add fallbacks for mandatory production secrets to prevent startup crash

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'test-admin-jwt-secret'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'test-api-token-salt'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'test-transfer-token-salt'),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('ENCRYPTION_KEY', 'test-encryption-key'),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
